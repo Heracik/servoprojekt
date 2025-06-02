63d30eb7780e39c4584c547da6f513ca7b1c2030
Heracik <lukashorak2907@gmail.com>
2025-06-02 19:48:01 +0200

    update-hastagy

--- mqtt_subscriber/views.py
+++ mqtt_subscriber/views.py
@@ -38,7 +38,7 @@ cities_timezones = {
 
 
 def home(request):
-    # Pre každé mesto kapitalizujeme názov
+    
     capitalized_cities_timezones = {city.capitalize(): timezone for city, timezone in cities_timezones.items()}
     return render(request, 'mqtt_subscriber/home.html', {'cities_timezones': capitalized_cities_timezones})
 
@@ -49,14 +49,14 @@ def get_time_for_city(city, timezone):
         city_time = datetime.now(tz).strftime("%H:%M")
         return city_time
     except pytz.UnknownTimeZoneError:
-        return None  # Ak je časové pásmo neznáme, vráti None
+        return None  #
 
 def send_time_for_city(request, city, timezone):
-    # Zavoláme funkciu na získanie času pre dané mesto a jeho časové pásmo
+    
     city_time = get_time_for_city(city, timezone)
     
     if city_time:
-        # Odošleme čas na MQTT
+        
         send_current_time(city)  
         return JsonResponse({'message': f'Čas pre {city.capitalize()} ({timezone}) bol úspešne odoslaný! Čas: {city_time}'})
     else:
@@ -66,25 +66,25 @@ def send_time(request, city):
     send_current_time(city)
     return JsonResponse({'message': f'Time for {city} has been sent successfully!'})
 
-# Existujúce funkcie
+
 async def send_mqtt_message(custom_time):
-    # Vytvorenie MQTT klienta
+    
     client = MQTTClient("django_client")
 
-    # Callback pri pripojení
+    
     def on_connect(client, flags, rc, properties):
         print(f"Pripojenie úspešné, kód: {rc}")
 
     try:
-        # Pripojenie k Hivemq brokeru
+        
         await client.connect('broker.emqx.io', 1883)
         print("Pripojené k MQTT brokeru")
 
-        # Topic pre odoslanie času
+       
         topic = "esp32/projekt1"
         message = f"{custom_time}"
 
-        # Odoslanie správy do topicu
+        
         await client.publish(topic, message)
         print(f"Správa '{message}' odoslaná do témy '{topic}'")
 
@@ -98,20 +98,20 @@ async def send_mqtt_message(custom_time):
 async def send_custom_time(request):
     if request.method == 'POST':
         try:
-            # Načítanie dát z požiadavky
+            
             data = json.loads(request.body)
             custom_time = data.get('time')
 
-            # Kontrola, či je hodnota 'time' prítomná
+            
             if not custom_time:
                 return JsonResponse({"message": "Chýba hodnota 'time'."}, status=400)
 
-            # Volanie asynchrónnej funkcie
+            
             await send_mqtt_message(custom_time)
 
             return JsonResponse({"message": f"Vlastný čas {custom_time} bol odoslaný na MQTT!"})
         except Exception as e:
-            # Logovanie chyby
+            
             print(f"Chyba pri odosielaní vlastného času: {e}")
             return JsonResponse({"message": "Nastala chyba pri odosielaní času."}, status=500)
 
@@ -136,7 +136,7 @@ async def send_shutdown_command():
 def send_shutdown_command_view(request):
     if request.method == 'POST':
         try:
-            # Použitie asyncio.run() na správne vykonanie asynchrónnej funkcie v Django synchrónnom kontexte
+            
             asyncio.run(send_shutdown_command())
             return JsonResponse({'status': 'success', 'message': 'Príkaz na vypnutie bol odoslaný!'})
         except Exception as e:
@@ -145,7 +145,7 @@ def send_shutdown_command_view(request):
 
     return JsonResponse({'status': 'error', 'message': 'Nesprávna požiadavka!'}, status=400)
 
-# Nové funkcie pre synchronizáciu a desynchronizáciu
+
 async def send_sync_command():
     client = MQTTClient("sync-client")
 
